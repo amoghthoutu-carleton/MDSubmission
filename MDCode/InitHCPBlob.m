3c5452d9f1270db9ea45b76b625155a25d587d56
amoghthotutu-carleton <amoghthoutu@cmail.carleton.ca>
2021-01-22 16:17:55 -0500

    PA0

---
+++ MDCode/InitHCPBlob.m
@@ -0,0 +1,29 @@
+doPlot = 1;
+dt = 15e-15;
+TStop = 1000 * dt;
+InitDist = 0.05;
+Method = 'VE'; % VE -- verlot; FD -- Forward Difference
+
+Mass0 = 14 * C.am; % Silicon
+Mass1 = 4 * C.am; % Argon
+
+AtomSpacing = 0.5430710e-9;
+LJSigma = AtomSpacing / 2^(1 / 6);
+LJEpsilon = 1e-21;
+
+PhiCutoff = 3 * AtomSpacing * 1.1;
+
+T = 30;
+
+AddHCPAtomicBlob(10, 0, 0, 0, 0, 0, InitDist, T, randi([0 1],1,1));
+
+Size = 15 * AtomSpacing;
+Limits = [-Size +Size -Size +1.5*Size]; % square is good
+PlDelt = 5 * dt;
+MarkerSize = 4;
+PlotFile = 'HCPBlob.gif';
+doPlotImage = 1;
+PlotSize = [100, 100, 1049, 1549];
+
+ScaleV = .2e-11;
+ScaleF = 20;
